fef7c161dc358b424b6ba79d420adfaa69d8e2c4
Mujahidul12628 <mujahidul15-12628@diu.edu.bd>
2023-10-06 21:46:11 +0600

    Project Dependencies

---
+++ src/assets/common/Login.jsx
@@ -0,0 +1,12 @@
+/* eslint-disable no-unused-vars */
+import React from 'react';
+
+const login = () => {
+    return (
+        <div>
+            <h1>Login</h1>
+        </div>
+    );
+};
+
+export default login;
